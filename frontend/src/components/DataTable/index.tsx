3d855bb42d8e347e461e70087b49034a1543435d
Wendell Rodrigues <robertowendellrodrigues@gmail.com>
2021-09-07 15:55:47 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,49 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+    <table className="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes visitados</th>
+                <th>Negócios fechados</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Roberto</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Wescley</td>
+                <td>34</td>
+                <td>22</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>21</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Wendell</td>
+                <td>34</td>
+                <td>20</td>
+                <td>15017.00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+    );
+}
+
+export default DataTable;
