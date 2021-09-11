018bd2b0eae049f340f3563b238ea728a2145711
Wendell Rodrigues <robertowendellrodrigues@gmail.com>
2021-09-11 14:11:51 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -21,13 +21,10 @@ const DonutChart = () => {
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries });
-               console.log(chartData);
+               //console.log(chartData);
             });
     }, []);
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+   
     const options = {
         legend: {
             show: true
